69906340743d91e80a5642789daacf1bd018bff4
Barış Akşipal <b.aksipal@gmail.com>
2021-09-23 16:53:32 +0300

    sa

--- src/main/java/com/biletdeneme/biletdeneme/api/KullaniciApi.java
+++ src/main/java/com/biletdeneme/biletdeneme/api/KullaniciApi.java
@@ -1,9 +1,15 @@
 package com.biletdeneme.biletdeneme.api;
 
+import com.biletdeneme.biletdeneme.modal.Kullanici;
+import com.biletdeneme.biletdeneme.modal.Sehir;
 import com.biletdeneme.biletdeneme.service.KullaniciService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bilet")
@@ -12,14 +18,14 @@ public class KullaniciApi {
     @Autowired
     private KullaniciService kullaniciService;
 
-
     public KullaniciApi(KullaniciService kullaniciService) {
         this.kullaniciService = kullaniciService;
     }
 
-
-
-
+    @GetMapping("/get-all")
+    public ResponseEntity<List<Kullanici>> getAll(){
+        return ResponseEntity.ok(kullaniciService.getAll());
+    }
 
 
 
--- src/main/java/com/biletdeneme/biletdeneme/service/KullaniciService.java
+++ src/main/java/com/biletdeneme/biletdeneme/service/KullaniciService.java
@@ -1,16 +1,30 @@
 package com.biletdeneme.biletdeneme.service;
 
+import com.biletdeneme.biletdeneme.modal.Kullanici;
+import com.biletdeneme.biletdeneme.repository.KullaniciRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Slf4j
 @Service
 @Transactional
 public class KullaniciService {
 
+    @Autowired
+    private KullaniciRepository kullaniciRepository;
+
+    public KullaniciService(KullaniciRepository kullaniciRepository) {
+        this.kullaniciRepository = kullaniciRepository;
+    }
 
 
+    public List<Kullanici> getAll() {
+    return kullaniciRepository.findAll();
+
+    }
 
 }
